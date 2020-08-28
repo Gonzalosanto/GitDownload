29ec50882e0d5848f5d0097c7d78b4d201341704
gonzalosanto <sancruzgon@gmail.com>
2020-08-28 11:44:56 -0300

    FIX de rutas de carpetas y archivos en saveJsonFile

--- scripts.js
+++ scripts.js
@@ -5,53 +5,58 @@ const fs= require('fs')
 const path = require('path');
  
 // ESCRIBE EL ARCHIVO EN LA RUTA CON EL NOMBRE Y FORMATO LA DATA QUE SE DESCARGA DEL GITHUB 
-function saveJsonFile(data, route, filename){
+function saveJsonFile(data, route){
   return new Promise( (resolve, reject)=>{
+    
+    let pathRoute = route.split('/');
+    let filename = pathRoute.pop();
+    let path = pathRoute.join('/')
       try {
-        if(!fs.existsSync(route)){
-            fs.mkdirSync(route, {recursive: true}, (err) => {
+        if(!fs.existsSync(path)){
+            fs.mkdirSync(path, {recursive: true}, (err) => {
                if (err) throw err;          
               });
-            fs.writeFileSync(route+'/'+filename, data, function(err) {
-            if(err) {
-                reject(err);
-            }
-            resolve('ok');
-            console.log("Archivo descargado en la carpeta nueva");
+            fs.writeFile(path +'/'+filename, data, function(err) {
+              if(err) {
+                  reject(err);
+              }
+              //console.log("Archivo descargado en la carpeta nueva");
+              resolve('ok');              
             });  
               
         }else {
-            console.log('La carpeta ya existe en la ruta seleccionada');
-            fs.writeFile(route+'/'+filename, data, function(err) {
+            //console.log('La carpeta ya existe en la ruta seleccionada');
+            fs.writeFile(path+'/'+filename, data, function(err) {
               if(err) {
                 reject(err);
               }
               resolve('ok');
-              console.log("Archivo descargado en la carpeta existente");
+              //console.log("Archivo descargado en la carpeta existente");
             }); 
-        }
-          
-      } catch (error) {
+        }          
+      } 
+      catch (error) {
           reject(error);
       }
   });
 }
 
 // TODO: Configurar para recibir una carpeta con archivos e iterar sobre los archivos
-function getFileFromGithub(owner, repository, file){
+function getFileFromGithub(owner, repository, path){
   return new Promise( (resolve, reject)=>{   
-    const apiUrl = "https://api.github.com";
-    let endpoint= "/repos/"+owner+"/"+repository+"/contents/"+file;
+    const apiUrl = "https://api.github.com/";
+    let endpoint= "repos/"+owner+"/"+repository+"/contents/"+path;
 
     fetch(apiUrl + endpoint, {method:'GET', headers: {'Accept':'application/vnd.github.v3+json', 'Authorization' : 'Bearer ' + config.OAuthToken }})
     .then(response => response.json())
     .then((json)=>{
+      console.log(json);
       for (const i in json) {
         console.log(json[i]);
         fetch(json[i]['download_url'])
         .then(res=>res.text())
         .then((data)=>{        
-          saveJsonFile(data, json[i]['path'], json[i]['name']);
+          saveJsonFile(data, json[i]['path']);
           resolve(data);
         })
         .catch((err) => {
